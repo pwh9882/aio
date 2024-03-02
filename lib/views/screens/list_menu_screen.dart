bea271defff577fa032c7c233b03e51cdf82c2ab
pwh9882 <pwh9882@gmail.com>
2024-03-03 01:59:59 +0900

    Add flutter_zoom_drawer package and implement list drawer functionality Upgrading Flutter to 3.19.2 from 3.7.7

---
+++ lib/views/screens/list_menu_screen.dart
@@ -0,0 +1,14 @@
+import 'package:aio/controllers/list_drawer_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ListMenuScreen extends GetView<ListDrawerController> {
+  const ListMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
