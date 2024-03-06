b6c3756d0c1cd592f7d8a8b54363efda43324140
pwh9882 <pwh9882@gmail.com>
2024-03-07 02:18:03 +0900

    Refactor custom main drawer widget and add GetX state management

--- lib/views/widgets/custom_main_drawer.dart
+++ lib/views/widgets/custom_main_drawer.dart
@@ -1,8 +1,21 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainDrawerController extends GetxController {
+  var offset = 0.0.obs;
+
+  void updateOffset(double newOffset) {
+    offset.value = newOffset;
+  }
+
+  void resetOffset() {
+    offset.value = 0.0;
+  }
+}
 
 class CustomMainDrawer extends StatefulWidget {
-  const CustomMainDrawer({Key? key}) : super(key: key);
+  const CustomMainDrawer({super.key});
 
   @override
   CustomMainDrawerState createState() => CustomMainDrawerState();
@@ -10,22 +23,21 @@ class CustomMainDrawer extends StatefulWidget {
 
 class CustomMainDrawerState extends State<CustomMainDrawer>
     with SingleTickerProviderStateMixin {
-  double _offset = 0.0;
   late AnimationController _animationController;
   late Animation<double> _animation;
+  final MainDrawerController controller = Get.put(MainDrawerController());
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 250),
+      duration: const Duration(milliseconds: 300),
     );
-    _animation = Tween<double>(begin: 0, end: 0).animate(_animationController)
+    _animation = Tween<double>(begin: 0, end: 0).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeOut))
       ..addListener(() {
-        setState(() {
-          _offset = _animation.value;
-        });
+        controller.updateOffset(_animation.value);
       });
   }
 
@@ -38,31 +50,34 @@ class CustomMainDrawerState extends State<CustomMainDrawer>
         _animationController.stop();
       },
       onHorizontalDragUpdate: (details) {
-        // debugPrint("offset: $_offset");
-        setState(() {
-          _offset =
-              math.max(0.0, math.min(_offset + details.delta.dx, screenWidth));
-        });
+        double newOffset = math.max(0.0,
+            math.min(controller.offset.value + details.delta.dx, screenWidth));
+        controller.updateOffset(newOffset);
       },
       onHorizontalDragEnd: (details) {
-        if (_offset > 10) {
+        if (controller.offset.value > 10) {
           Navigator.pop(context);
         }
-        _animation = Tween<double>(begin: _offset, end: 0.0)
-            .animate(_animationController);
+        _animation = Tween<double>(begin: controller.offset.value, end: 0)
+            .animate(CurvedAnimation(
+                parent: _animationController, curve: Curves.easeOut))
+          ..addListener(() {
+            controller.updateOffset(_animation.value);
+          });
         _animationController.forward(from: 0.0);
       },
-      child: Transform.translate(
-        offset: Offset(_offset, 0.0),
-        child: Drawer(
-          // Drawer contents go here
-          width: screenWidth,
-          child: const Column(
-            children: [
-              Text('Item 1'),
-              Text('Item 2'),
-              Text('Item 3'),
-            ],
+      child: Obx(
+        () => Transform.translate(
+          offset: Offset(controller.offset.value, 0.0),
+          child: Drawer(
+            width: screenWidth,
+            child: const Column(
+              children: [
+                Text('Item 1'),
+                Text('Item 2'),
+                Text('Item 3'),
+              ],
+            ),
           ),
         ),
       ),
