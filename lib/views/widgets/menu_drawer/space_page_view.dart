def5a19ac59c47ffaf40c8a3cd6433fa91659760
pwh9882 <pwh9882@gmail.com>
2024-03-07 23:44:27 +0900

    Refactor SpacePageView and add SpacePageViewController (using getx, statelesswidget)

--- lib/views/widgets/menu_drawer/space_page_view.dart
+++ lib/views/widgets/menu_drawer/space_page_view.dart
@@ -1,45 +1,56 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SpacePageView extends StatefulWidget {
+class SpacePageViewController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  late PageController pageViewController;
+  late TabController tabController;
+  var currentPageIndex = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    pageViewController = PageController();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void onClose() {
+    pageViewController.dispose();
+    tabController.dispose();
+    super.onClose();
+  }
+
+  void updateCurrentPageIndex(int index) {
+    currentPageIndex.value = index;
+    tabController.index = index;
+  }
+
+  void animateToPage(int index) {
+    pageViewController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+    updateCurrentPageIndex(index);
+  }
+}
+
+class SpacePageView extends StatelessWidget {
   const SpacePageView({super.key});
 
   @override
-  State<SpacePageView> createState() => _SpacePageViewState();
-}
-
-class _SpacePageViewState extends State<SpacePageView>
-    with TickerProviderStateMixin {
-  late PageController _pageViewController;
-  late TabController _tabController;
-  int _currentPageIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageViewController = PageController();
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageViewController.dispose();
-    _tabController.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final SpacePageViewController controller =
+        Get.put(SpacePageViewController());
     final TextTheme textTheme = Theme.of(context).textTheme;
 
     return Stack(
       alignment: Alignment.bottomCenter,
       children: <Widget>[
         PageView(
-          /// [PageView.scrollDirection] defaults to [Axis.horizontal].
-          /// Use [Axis.vertical] to scroll vertically.
-          controller: _pageViewController,
-          onPageChanged: _handlePageViewChanged,
+          controller: controller.pageViewController,
+          onPageChanged: controller.updateCurrentPageIndex,
           children: <Widget>[
             Center(
               child: Text('First Page', style: textTheme.titleLarge),
@@ -52,72 +63,19 @@ class _SpacePageViewState extends State<SpacePageView>
             ),
           ],
         ),
-        PageIndicator(
-          tabController: _tabController,
-          currentPageIndex: _currentPageIndex,
-          onUpdateCurrentPageIndex: _updateCurrentPageIndex,
-          isOnDesktopAndWeb: _isOnDesktopAndWeb,
-        ),
+        PageIndicator(controller: controller),
       ],
     );
-  }
-
-  void _handlePageViewChanged(int currentPageIndex) {
-    if (!_isOnDesktopAndWeb) {
-      return;
-    }
-    _tabController.index = currentPageIndex;
-    setState(() {
-      _currentPageIndex = currentPageIndex;
-    });
-  }
-
-  void _updateCurrentPageIndex(int index) {
-    _tabController.index = index;
-    _pageViewController.animateToPage(
-      index,
-      duration: const Duration(milliseconds: 400),
-      curve: Curves.easeInOut,
-    );
-  }
-
-  bool get _isOnDesktopAndWeb {
-    return true;
-    if (kIsWeb) {
-      return true;
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.macOS:
-      case TargetPlatform.linux:
-      case TargetPlatform.windows:
-        return true;
-      case TargetPlatform.android:
-      case TargetPlatform.iOS:
-      case TargetPlatform.fuchsia:
-        return false;
-    }
   }
 }
 
 class PageIndicator extends StatelessWidget {
-  const PageIndicator({
-    super.key,
-    required this.tabController,
-    required this.currentPageIndex,
-    required this.onUpdateCurrentPageIndex,
-    required this.isOnDesktopAndWeb,
-  });
+  const PageIndicator({super.key, required this.controller});
 
-  final int currentPageIndex;
-  final TabController tabController;
-  final void Function(int) onUpdateCurrentPageIndex;
-  final bool isOnDesktopAndWeb;
+  final SpacePageViewController controller;
 
   @override
   Widget build(BuildContext context) {
-    if (!isOnDesktopAndWeb) {
-      return const SizedBox.shrink();
-    }
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
     return Padding(
@@ -125,39 +83,35 @@ class PageIndicator extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          IconButton(
-            splashRadius: 16.0,
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              if (currentPageIndex == 0) {
-                return;
-              }
-              onUpdateCurrentPageIndex(currentPageIndex - 1);
-            },
-            icon: const Icon(
-              Icons.arrow_left_rounded,
-              size: 32.0,
-            ),
-          ),
+          Obx(() => IconButton(
+                splashRadius: 16.0,
+                padding: EdgeInsets.zero,
+                onPressed: controller.currentPageIndex.value == 0
+                    ? null
+                    : () => controller
+                        .animateToPage(controller.currentPageIndex.value - 1),
+                icon: const Icon(
+                  Icons.arrow_left_rounded,
+                  size: 32.0,
+                ),
+              )),
           TabPageSelector(
-            controller: tabController,
+            controller: controller.tabController,
             color: colorScheme.background,
             selectedColor: colorScheme.primary,
           ),
-          IconButton(
-            splashRadius: 16.0,
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              if (currentPageIndex == 2) {
-                return;
-              }
-              onUpdateCurrentPageIndex(currentPageIndex + 1);
-            },
-            icon: const Icon(
-              Icons.arrow_right_rounded,
-              size: 32.0,
-            ),
-          ),
+          Obx(() => IconButton(
+                splashRadius: 16.0,
+                padding: EdgeInsets.zero,
+                onPressed: controller.currentPageIndex.value == 2
+                    ? null
+                    : () => controller
+                        .animateToPage(controller.currentPageIndex.value + 1),
+                icon: const Icon(
+                  Icons.arrow_right_rounded,
+                  size: 32.0,
+                ),
+              )),
         ],
       ),
     );
